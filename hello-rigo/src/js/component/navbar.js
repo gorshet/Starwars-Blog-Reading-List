9be21d5f6380611f026921cd1bb92d433bcab431
gorshet <jorgemontes771@gmail.com>
2019-08-11 01:07:52 +0000

    fixing errors

--- hello-rigo/src/js/component/navbar.js
+++ hello-rigo/src/js/component/navbar.js
@@ -4,14 +4,18 @@ import { Link } from "react-router-dom";
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light mb-3">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+			<nav className="navbar navbar-light bg-light">
+				<img src="https://i.dlpng.com/static/png/116175_preview.png" className="logo" />
+				<div className="dropdown">
+					<button
+						className="btn btn-primary dropdown-toggle"
+						type="button"
+						id="dropdownMenuButton"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false">
+						Favorites
+					</button>
 				</div>
 			</nav>
 		);
--- hello-rigo/src/js/store/flux.js
+++ hello-rigo/src/js/store/flux.js
@@ -1,34 +1,16 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			planets: [],
+			favorites: []
 		},
 		actions: {
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getCharacter: (name, id) => {},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			getPlanet: (name, id) => {},
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			markFavorite: character => {}
 		}
 	};
 };
