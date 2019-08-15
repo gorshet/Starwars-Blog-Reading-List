de99d91b46911faa3136a454107999d2f515c0be
gorshet <jorgemontes771@gmail.com>
2019-08-15 21:51:22 +0000

    needs styles for buttons

--- hello-rigo/src/js/component/navbar.js
+++ hello-rigo/src/js/component/navbar.js
@@ -20,7 +20,9 @@ export class Navbar extends React.Component {
 	render() {
 		return (
 			<nav className="navbar navbar-light bg-light">
-				<img src="https://i.dlpng.com/static/png/116175_preview.png" className="logo" />
+				<Link to={"/"}>
+					<img src="https://i.dlpng.com/static/png/116175_preview.png" className="logo" />
+				</Link>
 				<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
 					<Context.Consumer>
 						{({ store, actions }) => {
